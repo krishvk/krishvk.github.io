398247db37633f83f6557f059b5e97cebf81c7ad
Vijaya Krishna Kasula <krishna.vijay4444@gmail.com>
2020-04-26 19:03:37 +0530

    try analytics

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,36 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_material'
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Project Name',
+
+    # Set you GA account ID to enable tracking
+    'google_analytics_account': 'UA-36650734-2',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://krishkasula.gitlab.io',
+
+    # Set the color and the accent color
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://gitlab.com/krishkasula/krishkasula.gitlab.io',
+    'repo_name': 'Krishna',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,7 +84,3 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 master_doc = 'index'
-
-def setup(app):
-    '''Use customizations'''
-    app.add_css_file('my_theme.css')
